f95c6020807092bf5542d39c9b67c344e0884447
neozvbambah <neozvbambah@gmail.com>
2023-08-21 19:26:03 +0530

    arrangment of components

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -1,9 +1,8 @@
 export function Footer() {
     return (
-        <div className="flex">
+        <div className="flex gap-x-3 absolute bottom-0">
             <button className="bg-prim text-sec p-3 rounded-lg w-48 font-extrabold text-2xl">My Github</button>
             <button className="bg-prim text-sec p-3 rounded-lg w-48 font-extrabold text-2xl">Other apps</button>
-
         </div>
     )
 }
--- src/app/components/main.js
+++ src/app/components/main.js
@@ -1,9 +1,9 @@
 export function Main() {
     return (
-        <div className="mt-40">
-            <h1 className="text-2xl text-prim font-bold">Remaining Amount:</h1>
-            <h2 className="text-white font-bold text-6xl ml-auto">₹65</h2>
-            <button className="bg-prim text-sec p-3 rounded-lg w-48 font-extrabold text-2xl">New Expense</button>
+        <div className="flex flex-col mt-40">
+            <h1 className="text-2xl text-prim font-bold self-center">Remaining Amount:</h1>
+            <h2 className="text-white font-bold text-6xl self-center mt-9">₹65</h2>
+            <button className="bg-prim text-sec p-3 rounded-lg w-48 font-extrabold text-2xl self-center mt-16">New Expense</button>
         </div>
     )
 }
